6b932d6d59177c5084209ff8f28c7aa9c1e5c37d
Evgeniy Mukhin <eugmukhin@yandex.ru>
2020-01-10 17:21:29 +0300

    Add startaup script

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O - https://raw.githubusercontent.com/Otus-DevOps-2019-11/eugmukhin_infra/cloud-testapp/install_ruby.sh | bash
+wget -O - https://raw.githubusercontent.com/Otus-DevOps-2019-11/eugmukhin_infra/cloud-testapp/install_mongodb.sh | bash
+wget -O - https://raw.githubusercontent.com/Otus-DevOps-2019-11/eugmukhin_infra/cloud-testapp/deploy.sh | bash
